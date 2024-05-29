6f86e3dbca77d617e3c841b1de7bfeafa663ad75
clotildebeckerlamoureux <clobecker77@gmail.com>
2024-05-29 15:49:23 +0200

    structure and layout of both components done

--- src/app/components/globalRecommendation/global-recommendation/global-recommendation.component.ts
+++ src/app/components/globalRecommendation/global-recommendation/global-recommendation.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardGameSmallComponent } from '../../cardGameSmall/card-game-small/card-game-small.component';
 
 @Component({
   selector: 'app-global-recommendation',
   standalone: true,
-  imports: [],
+  imports: [CardGameSmallComponent],
   templateUrl: './global-recommendation.component.html',
   styleUrl: './global-recommendation.component.scss'
 })
