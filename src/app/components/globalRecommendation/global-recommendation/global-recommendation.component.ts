465a31c86679a220b90fadc7bdb50a04fe3c7b1a
clotildebeckerlamoureux <clobecker77@gmail.com>
2024-05-29 14:49:29 +0200

    components created

---
+++ src/app/components/globalRecommendation/global-recommendation/global-recommendation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-global-recommendation',
+  standalone: true,
+  imports: [],
+  templateUrl: './global-recommendation.component.html',
+  styleUrl: './global-recommendation.component.scss'
+})
+export class GlobalRecommendationComponent {
+
+}
