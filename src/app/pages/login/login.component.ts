a34c40c1b0a2400b11f73dbab41d6771ad8e1c4e
Sid-Ahmed7 <sido91080@hotmail.fr>
2024-06-03 10:04:53 +0200

    Add CommonModule, FormsModule, and RouterLink imports

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
