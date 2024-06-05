11724cec796881f3d1919d0919c92cc7e354ec89
Sid-Ahmed7 <sido91080@hotmail.fr>
2024-06-05 17:18:49 +0200

    Add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ItemsComponent } from './pages/items/items.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 export const routes: Routes = [
   {
@@ -15,5 +16,9 @@ export const routes: Routes = [
   {
     path: 'items',
     component: ItemsComponent,
+  },
+  {
+    path: 'inscription',
+    component: SignUpComponent,
   },
 ];
