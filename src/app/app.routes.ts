c84326af554c3361a169c9683a19c711ba570a00
marwasliti <marwasliti11888@yahoo.com>
2024-06-06 16:28:34 +0200

    conflit resolved

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { CguComponent } from './pages/cgu/cgu.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AproposComponent } from './pages/apropos/apropos.component';
 
 export const routes: Routes = [
   {
@@ -11,4 +14,17 @@ export const routes: Routes = [
     redirectTo: '/home',
     pathMatch: 'full',
   },
+  {
+    path:'cgu',
+    component: CguComponent,
+  },
+  {
+    path:'contact',
+    component: ContactComponent,
+  },
+ {
+    path:'apropos',
+    component: AproposComponent,
+  },
+
 ];
