d58918678cc068d822b1a443b7ba607fc7f7f210
Ergy Zenzo <ergyzenzo.dev@gmail.com>
2024-06-06 16:30:50 +0200

    fixed responsive home page added last games component added isConnected function ( set true to display the last game for the user)

---
+++ src/app/last-game-home/last-game-home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CardGameSmallComponent } from '../components/cardGameSmall/card-game-small/card-game-small.component';
+
+@Component({
+  selector: 'app-last-game-home',
+  standalone: true,
+  imports: [CardGameSmallComponent],
+  templateUrl: './last-game-home.component.html',
+  styleUrl: './last-game-home.component.scss'
+})
+export class LastGameHomeComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,12 +4,18 @@ import { RouterLink } from '@angular/router';
 import { HomepageBannerComponent } from '../../homepage-banner/homepage-banner.component';
 
 import { GlobalRecommendationComponent } from '../../components/globalRecommendation/global-recommendation/global-recommendation.component';
+import { LastGameHomeComponent } from '../../last-game-home/last-game-home.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, HomepageBannerComponent, GlobalRecommendationComponent],
+  imports: [RouterLink, HomepageBannerComponent, GlobalRecommendationComponent, LastGameHomeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+
+  isConnected(){
+    return !true;
+  }
+}
