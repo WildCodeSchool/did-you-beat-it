160c8f43fce30875de865e8c31d67d96fdf98c49
Sid-Ahmed7 <sido91080@hotmail.fr>
2024-06-06 17:24:07 +0200

    Add variable for button text

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { CustomButtonComponent } from '../../components/custom-button/custom-button.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterLink],
+  imports: [CommonModule, FormsModule, RouterLink, CustomButtonComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
+btnText = 'Login';
 }
