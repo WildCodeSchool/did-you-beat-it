994066e20853445a54dfff490306ae1b45f81683
Sid-Ahmed7 <sido91080@hotmail.fr>
2024-06-06 13:37:59 +0200

    Delete import signupcomponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 
 export const routes: Routes = [
