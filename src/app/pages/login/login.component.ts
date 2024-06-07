68aa5eb4826583cfd297df80197ead7bf2c67dd1
Sid-Ahmed7 <sido91080@hotmail.fr>
2024-06-07 09:47:34 +0200

    Add interface for User

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,7 +3,10 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { CustomButtonComponent } from '../../components/custom-button/custom-button.component';
-
+interface UserLogin {
+  email : string;
+  password:string;
+}
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -13,4 +16,9 @@ import { CustomButtonComponent } from '../../components/custom-button/custom-but
 })
 export class LoginComponent {
 btnText = 'Connexion';
+
+newUser: UserLogin = {
+  email: "",
+  password: "",
+}
 }
