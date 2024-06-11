75f73b1c84cc176f5e38b33e9bd3b93fa7c829db
Ergy Zenzo <ergyzenzo.dev@gmail.com>
2024-06-11 16:09:16 +0200

    added links CTA on the homepage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,11 +5,12 @@ import { HomepageBannerComponent } from '../../components/homepage-banner/homepa
 
 import { GlobalRecommendationComponent } from '../../components/globalRecommendation/global-recommendation/global-recommendation.component';
 import { LastGameHomeComponent } from '../../components/last-game-home/last-game-home.component';
+import { CustomButtonComponent } from '../../components/custom-button/custom-button.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, HomepageBannerComponent, GlobalRecommendationComponent, LastGameHomeComponent],
+  imports: [RouterLink, HomepageBannerComponent, GlobalRecommendationComponent, LastGameHomeComponent, CustomButtonComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
@@ -18,4 +19,6 @@ export class HomeComponent {
   isConnected(){
     return !true;
   }
+
+  textButton = 'Découvrir les jeux';
 }
