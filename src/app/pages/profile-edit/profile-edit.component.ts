fb580facb5aa165e67f7df30b018adf29da2afaf
clotildebeckerlamoureux <clobecker77@gmail.com>
2024-06-12 15:45:34 +0200

    side navigation structure

--- src/app/pages/profile-edit/profile-edit.component.ts
+++ src/app/pages/profile-edit/profile-edit.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-profile-edit',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './profile-edit.component.html',
   styleUrl: './profile-edit.component.scss'
 })
