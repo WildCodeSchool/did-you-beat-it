3b38c8205a5dbc55fd51ba56fc87545ad15629a3
clotildebeckerlamoureux <clobecker77@gmail.com>
2024-06-12 14:49:42 +0200

    page and route created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { CguComponent } from './pages/cgu/cgu.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { AproposComponent } from './pages/apropos/apropos.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { ProfileEditComponent } from './pages/profile-edit/profile-edit.component';
 
 
 
@@ -38,4 +39,8 @@ export const routes: Routes = [
     path:'apropos',
     component: AproposComponent,
   },
+  {
+    path:'edit',
+    component: ProfileEditComponent,
+  }
 ];
