c59002a2db0a1247545bb05c3e4244b14eb0d673
Sid-Ahmed7 <sido91080@hotmail.fr>
2024-06-12 14:10:03 +0200

    Add tab-nav component

---
+++ src/app/components/tab-nav/tab-nav.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-tab-nav',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './tab-nav.component.html',
+  styleUrl: './tab-nav.component.scss'
+})
+export class TabNavComponent {
+  
+  currentSection: string = 'liste-des-jeux';
+
+  showSection(section: string) {
+    this.currentSection = section;
+  }
+
+
+}
