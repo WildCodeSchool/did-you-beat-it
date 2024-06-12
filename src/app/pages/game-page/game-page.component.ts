7acd2e51465445dc7e964db60e76d2239365038e
Ergy Zenzo <ergyzenzo.dev@gmail.com>
2024-06-12 15:22:00 +0200

    added html structure

--- src/app/pages/game-page/game-page.component.ts
+++ src/app/pages/game-page/game-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HomepageBannerComponent } from '../../components/homepage-banner/homepage-banner.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-game-page',
   standalone: true,
-  imports: [],
+  imports: [HomepageBannerComponent, RouterLink],
   templateUrl: './game-page.component.html',
   styleUrl: './game-page.component.scss'
 })
