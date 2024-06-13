a1f5830ee53081d82e1c525e67234e80e65f2386
clotildebeckerlamoureux <clobecker77@gmail.com>
2024-06-13 14:40:09 +0200

    dynamic style for navigation links

--- src/app/pages/profile-edit/profile-edit.component.ts
+++ src/app/pages/profile-edit/profile-edit.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { EditInformationsComponent } from '../../components/edit-informations/edit-informations.component';
 
 @Component({
   selector: 'app-profile-edit',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, EditInformationsComponent],
+  imports: [RouterLink, RouterOutlet, RouterLinkActive, EditInformationsComponent],
   templateUrl: './profile-edit.component.html',
   styleUrl: './profile-edit.component.scss'
 })
