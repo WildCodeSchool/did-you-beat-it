b562d3ca4adbcb08eb0d8c8c1343c1e73f882ac1
Sid-Ahmed7 <sido91080@hotmail.fr>
2024-06-14 10:04:53 +0200

    remove import cardgamebig components

--- src/app/components/tab-nav/tab-nav.component.ts
+++ src/app/components/tab-nav/tab-nav.component.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { CardGameSmallComponent } from '../cardGameSmall/card-game-small/card-game-small.component';
-import { CardGameBigComponent } from '../card-game-big/card-game-big.component';
 
 
 @Component({
   selector: 'app-tab-nav',
   standalone: true,
-  imports: [CommonModule, CardGameSmallComponent, CardGameBigComponent],
+  imports: [CommonModule, CardGameSmallComponent],
   templateUrl: './tab-nav.component.html',
   styleUrl: './tab-nav.component.scss'
 })
