9ec7104f180417c84d2f89fe69c463baeb14d10b
Sid-Ahmed7 <sido91080@hotmail.fr>
2024-06-18 11:16:15 +0200

    Add method to reset filter

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,19 +1,38 @@
 import { CommonModule } from '@angular/common';
 import { Component} from '@angular/core';
 import { CustomButtonComponent } from '../custom-button/custom-button.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-filter',
   standalone: true,
-  imports: [CommonModule, CustomButtonComponent],
+  imports: [CommonModule, FormsModule, CustomButtonComponent],
   templateUrl: './filter.component.html',
   styleUrl: './filter.component.scss'
 })
 export class FilterComponent {
   filterVisible: boolean = true;
+  filters = {
+    enCours: false,
+    terminer: false,
+    plateforme: '',
+    genre: '',
+    score: '',
+    annee: ''
+  };;
 
   toggleFilters() {
     this.filterVisible = !this.filterVisible;
+  }
+  resetFilters() {
+    this.filters = {
+      enCours: false,
+      terminer: false,
+      plateforme: '',
+      genre: '',
+      score: '',
+      annee: ''
+    };
   }
 
 }
