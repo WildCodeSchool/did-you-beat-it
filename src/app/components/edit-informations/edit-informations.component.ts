50958a853e40c03161a250ed72089e6ce495a87e
clotildebeckerlamoureux <clobecker77@gmail.com>
2024-06-19 16:03:23 +0200

    modify profile form logic

--- src/app/components/edit-informations/edit-informations.component.ts
+++ src/app/components/edit-informations/edit-informations.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CustomButtonComponent } from '../custom-button/custom-button.component';
+
+type updateForm = {
+  username:string;
+  email:string;
+  biography:string,
+  password:string;
+  confirmPassword:string;
+  };
 
 @Component({
   selector: 'app-edit-informations',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, CustomButtonComponent],
   templateUrl: './edit-informations.component.html',
   styleUrl: './edit-informations.component.scss'
 })
 export class EditInformationsComponent {
+  textButton = 'Sauvegarder';
+
+  update : updateForm = {
+    username : '',
+    email : '',
+    biography: '',
+    password : '',
+    confirmPassword : '',
+  };
 
 }
