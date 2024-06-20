50169cb9b7a3495821f19a2944f793ddc042d537
Sid-Ahmed7 <sido91080@hotmail.fr>
2024-06-20 09:32:11 +0200

    Add filter component

--- src/app/components/tab-nav/tab-nav.component.ts
+++ src/app/components/tab-nav/tab-nav.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { CardGameSmallComponent } from '../cardGameSmall/card-game-small/card-game-small.component';
+import { FilterComponent } from '../filter/filter.component';
 
 
 @Component({
   selector: 'app-tab-nav',
   standalone: true,
-  imports: [CommonModule, CardGameSmallComponent],
+  imports: [CommonModule, FilterComponent, CardGameSmallComponent],
   templateUrl: './tab-nav.component.html',
   styleUrl: './tab-nav.component.scss'
 })
