e0b276c4ee4ecdff343010553205cc15731ac09c
Ergy Zenzo <ergyzenzo.dev@gmail.com>
2024-06-20 17:14:52 +0200

    added button for game completion

--- src/app/pages/game-page/game-page.component.ts
+++ src/app/pages/game-page/game-page.component.ts
@@ -6,11 +6,12 @@ import { Game } from '../../models/games-mock';
 import { GamesApiService } from '../../models/games-api.service';
 import { NgFor, NgIf } from '@angular/common';
 import { GameRecommendationComponent } from '../../components/game-recommendation/game-recommendation.component';
+import { CustomButtonComponent } from '../../components/custom-button/custom-button.component';
 
 @Component({
   selector: 'app-game-page',
   standalone: true,
-  imports: [HomepageBannerComponent, RouterLink, NgFor, NgIf, GameRecommendationComponent],
+  imports: [HomepageBannerComponent, RouterLink, NgFor, NgIf, GameRecommendationComponent, CustomButtonComponent],
   templateUrl: './game-page.component.html',
   styleUrl: './game-page.component.scss'
 })
@@ -25,4 +26,11 @@ export class GamePageComponent {
       this.game = GamesFromJSON;
     })
   }
+
+  isConnected(){
+    return true;
+  }
+
+  btnText = 'Terminé ?';
+  class = 'game__infos--btn';
 }
