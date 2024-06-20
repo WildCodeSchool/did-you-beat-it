cf503b2ee320e85668032513417df50b61b8ccbb
clotildebeckerlamoureux <clobecker77@gmail.com>
2024-06-20 14:16:58 +0200

    upload banner image structure and logic

--- src/app/components/edit-informations/edit-informations.component.ts
+++ src/app/components/edit-informations/edit-informations.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CustomButtonComponent } from '../custom-button/custom-button.component';
+import { HttpClient } from '@angular/common/http';
+import { throwError } from "rxjs";
 
 type updateForm = {
   username:string;
@@ -19,7 +21,11 @@ type updateForm = {
   styleUrl: './edit-informations.component.scss'
 })
 export class EditInformationsComponent {
-  textButton = 'Sauvegarder';
+
+  private http: HttpClient = inject(HttpClient);
+
+  textButton: string = 'Sauvegarder';
+  uploadButton: string = 'Sauvegarder l\'image';
 
   update : updateForm = {
     username : '',
@@ -29,7 +35,41 @@ export class EditInformationsComponent {
     confirmPassword : '',
   };
 
-  onFileSelected(event: any):any {
+  bannerImg: string = "";
 
+  status: "initial" | "uploading" | "success" | "fail" = "initial";
+  bannerFile: File | null = null;
+
+  onFileSelected(event:any) {
+    const file: File = event.target.files[0];
+    if (file) {
+      this.status = "initial";
+      this.bannerFile = file;
+    }
   }
+
+  onUpload() {
+    if (this.bannerFile) {
+      const formData = new FormData();
+  
+      formData.append('file', this.bannerFile, this.bannerFile.name);
+  
+      const upload$ = this.http.post("https://httpbin.org/post", formData);
+  
+      this.status = 'uploading';
+  
+      upload$.subscribe({
+        next: () => {
+          this.status = 'success';
+          console.log(this.status)
+        },
+        error: (error: any) => {
+          this.status = 'fail';
+          console.log(this.status)
+          return throwError(() => error);
+        },
+      });
+    }
+  }
+
 }
