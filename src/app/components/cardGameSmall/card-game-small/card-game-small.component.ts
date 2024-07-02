d81358d1d181a43766ce11317b8f0b702d311c81
Sid-Ahmed7 <sido91080@hotmail.fr>
2024-07-02 16:58:02 +0200

    add input

--- src/app/components/cardGameSmall/card-game-small/card-game-small.component.ts
+++ src/app/components/cardGameSmall/card-game-small/card-game-small.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -9,5 +9,8 @@ import { RouterLink } from '@angular/router';
   styleUrl: './card-game-small.component.scss'
 })
 export class CardGameSmallComponent {
-
+  @Input() gameName?: string = 'TITRE DU JEU TRES TRES LONG';
+  @Input() gameCover?: string = './assets/pictures/rdoudou.jpeg';
+  @Input() gameGenre?: string = 'Genre';
+  @Input() release?: string  = 'Date de sortie'; 
 }
