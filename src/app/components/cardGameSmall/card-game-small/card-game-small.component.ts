95d11eca44b505718e8d6d65a6a7abeb30394102
Ergy Zenzo <ergyzenzo.dev@gmail.com>
2024-07-10 11:21:46 +0200

    fixed upcoming releases

--- src/app/components/cardGameSmall/card-game-small/card-game-small.component.ts
+++ src/app/components/cardGameSmall/card-game-small/card-game-small.component.ts
@@ -10,8 +10,8 @@ import { RouterLink } from '@angular/router';
   styleUrl: './card-game-small.component.scss'
 })
 export class CardGameSmallComponent {
-  @Input() gameName?: string = 'TITRE DU JEU TRES TRES LONG';
-  @Input() gameCover?: string = './assets/pictures/rdoudou.jpeg';
+  @Input() gameName?: string = 'Jeu Non Trouvé';
+  @Input() gameCover?: string = './assets/pictures/default_cover.png';
   @Input() gameGenre?: string = 'Genre';
   @Input() release?: string = 'Date de sortie'; 
 }
